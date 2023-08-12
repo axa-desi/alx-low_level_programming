9d262f9bd8bf3f459914b9420d09e87d04755698
axa-desi <agyei22@gmail.com>
2023-08-12 10:31:46 +0000

    alphbert

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-  
    /**
-    * main - entry point.
+    * main - Entry point.
+    * @void: Null value
     * Description : Print alphoabert lowercase except q and e 
     * Return: Always 0
     */
