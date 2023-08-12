c1b69e38c419446be638a7a971b0360c14f7a272
axa-desi <agyei22@gmail.com>
2023-08-12 23:21:57 +0000

    0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers from 0 to 9 with comma and space
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x == '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
