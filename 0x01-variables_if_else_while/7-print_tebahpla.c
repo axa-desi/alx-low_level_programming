18e1b4f6b82c2c7c3be73c956cf5be7016832713
axa-desi <agyei22@gmail.com>
2023-08-12 22:33:39 +0000

    lower revers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	wihle (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
