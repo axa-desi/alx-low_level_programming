85585be9a63198538efc88fe7410bca5b42f09c3
axa-desi <agyei22@gmail.com>
2023-08-12 14:25:04 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * @void: Null value
+  *
+  * Description: print m=numbers in base 10
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		putcher(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
