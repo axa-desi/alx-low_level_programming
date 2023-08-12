117dac40132b535d1ca25dc9a4a973a37a039aa1
axa-desi <agyei22@gmail.com>
2023-08-12 11:57:08 +0000

    positive_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,24 @@
   * Description:state whathr is positive, negative or zero
   * Return: Always 0.
   */
+
 int main(void)
 {
 		int n;
+
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		/*your code goes here*/
 		if (n > 0)
+		{
 			printf("%d is positive\n",n);
-		else if (n <0)
+		}
+		else if (n < 0)
+		{
 			printf("%d is negetive\n",n);
+		}
 		else
-			printf("%d is zero\n",n)	
-		return (0;)
+		{
+			printf("%d is zero\n",n);
+		}
+		return((0);
 }
