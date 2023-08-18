638547b166979200d6944b8ba65845d02f7dd180
axa-desi <agyei22@gmail.com>
2023-08-18 23:20:43 +0000

    put_c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print a statement of multilingual puzzle with puts
+ * Return: Zero Value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return(0);
+}
