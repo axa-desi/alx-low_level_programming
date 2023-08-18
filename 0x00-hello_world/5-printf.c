f9a87bb4fa8d784c5a36bc3788896abda91090cc
axa-desi <agyei22@gmail.com>
2023-08-18 23:44:15 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print with printf a statment
+ * Return: Always zero
+ */
+int main(void)
+
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
