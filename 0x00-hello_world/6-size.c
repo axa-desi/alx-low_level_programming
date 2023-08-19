b997981d69e96f010406f6eb619ea51d4580a433
axa-desi <agyei22@gmail.com>
2023-08-19 00:54:57 +0000

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print allsizes of a computer
+ * Return: Always Zero
+ */
+int main(void)
+{
+	int x;
+	long int t;
+	long long int u;
+	char v;
+	float w;
+
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(v)));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(t);
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)size(u));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(w));
+	return (0);
+	}
+
