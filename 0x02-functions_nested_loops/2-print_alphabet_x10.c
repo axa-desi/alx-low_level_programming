bcc4334608d90c6c4db0faba0bdf91b22033a72e
axa-desi <agyei22@gmail.com>
2023-08-20 10:34:28 +0000

    alphabet_10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char b;
+	int x = 0;
+
+	while (x++ <= 9)
+	{
+		for (b = 'a'; b <= 'z'; b++);
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+	}
+}
