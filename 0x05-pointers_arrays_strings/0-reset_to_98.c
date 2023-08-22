5ed751d876e5949b7dc2375cf0d51afbd75b4a0d
axa-desi <agyei22@gmail.com>
2023-08-22 04:02:37 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * update value to point to 98
+ *
+ * @n: entry point
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writ the character c to stdout
+ * @c: print characters
+ *
+ * Return: 1 on success , -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
