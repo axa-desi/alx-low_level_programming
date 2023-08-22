e65aa769f860b2f5f12fe3b3deafde9307c3b202
axa-desi <agyei22@gmail.com>
2023-08-22 06:19:27 +0000

    9strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcy - copy a string
+ * @des: destination valuve
+ * @src: source value
+ * Return: point to destination
+ */
+char *_strcpy(char *dest, char *src)
+	{
+		int a;
+		for (a = 0; src[a] != '\0'; a++)
+		{
+			dest[a] = src[a];
+		}
+		dest[a++] = '\0';
+		return (dest);
+	}
