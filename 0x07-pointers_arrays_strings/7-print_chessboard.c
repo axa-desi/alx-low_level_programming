d05a2206ed49858aeec8c4fd10499200c0760815
axa-desi <agyei22@gmail.com>
2023-09-04 19:15:01 +0000

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * priknt_chessboard - prints chessboard set 2D array
+ * @a: 2D array
+ *
+ */
+void orint_chessboard(char (*a)[8])
+{
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
+	}
+}
