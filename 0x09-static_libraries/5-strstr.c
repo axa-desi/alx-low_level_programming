3411224d1066172288b06d5ba58f1ba4d9dfc7de
axa-desi <agyei22@gmail.com>
2023-09-10 22:43:19 +0000

    libmy

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strstr - Entry point
+ *@haystack: input
+ *@needle: input
+ *Return: 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *m = haystack;
+		char *p = needle;
+
+		while (*m == *p && *p != '\0')
+		{
+			m++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
