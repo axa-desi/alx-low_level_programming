5487450327319a688a5323ca6c6e5d935dd39a2c
axa-desi <agyei22@gmail.com>
2023-09-27 11:45:42 +0000

    free update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listp2 - frees a linked list
- * @head: head of a list.
+ * @head: head of a list
  *
  * Return: no return.
  */
@@ -16,7 +16,7 @@ void free_listp2(listp_t **head)
 		curr = *head;
 		while ((temp = curr) != NULL)
 		{
-			curr = curr->next;
+			curr = curr->next,
 			free(temp);
 		}
 		*head = NULL;
@@ -25,13 +25,13 @@ void free_listp2(listp_t **head)
 
 /**
  * free_listint_safe - frees a linked list.
- * @h: head of a list.
+ * @h: head of a list
  *
  * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
+	size_t nodes = 0;
 	listp_t *hptr, *new, *add;
 	listint_t *curr;
 
@@ -52,21 +52,21 @@ size_t free_listint_safe(listint_t **h)
 		while (add->next != NULL)
 		{
 			add = add->next;
-			if (*h == add->p)
+			if (*h == (listint_t *)(add->p))
 			{
 				*h = NULL;
 				free_listp2(&hptr);
-				return (nnodes);
+				return (nodes);
 			}
 		}
 
 		curr = *h;
 		*h = (*h)->next;
 		free(curr);
-		nnodes++;
+		nodes++;
 	}
 
 	*h = NULL;
 	free_listp2(&hptr);
-	return (nnodes);
+	return (nodes);
 }
